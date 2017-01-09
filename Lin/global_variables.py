47727a8a178e2e5acf196ba59da202fb7df380f0
optas <optas@stanford.edu>
2017-01-08 19:32:55 -0800

    add global_variables.py

---
+++ Lin/global_variables.py
@@ -0,0 +1,8 @@
+learning_rate = 0.0001
+batch_size = 10
+training_epochs = 200
+display_step = 1
+Npoint = 3000
+nframe = 12
+imagelen = 32
+chair_file_list = '/orions4-zfs/projects/lins2/Panos_Space/DATA/ShapeNetPointClouds/3000/no_segmentations/03001627'
