1432bb0e29ec7f8ffb920f44e6a23dd2e6f6b51b
Panos <optas@stanford.edu>
2017-01-30 23:09:43 -0800

    mdb

--- point_clouds/point_cloud_vae.py
+++ point_clouds/point_cloud_vae.py
@@ -87,7 +87,7 @@ class VariationalAutoencoder(object):
         """Train model based on mini-batch of input data.
         Return cost of mini-batch.
         """
-        _, cost,rec = self.sess.run((self.optimizer, self.cost,self.x_reconstr), feed_dict={self.x: X})
+        _, cost, rec = self.sess.run((self.optimizer, self.cost,self.x_reconstr), feed_dict={self.x: X})
         print("reconstruct")
         print(rec[0])
         return cost
