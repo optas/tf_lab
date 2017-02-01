5c72e1d7b0af23857f5897ef123962f0b28d73cf
Lin Shao <lins2@oriong.stanford.edu>
2017-02-01 15:38:45 -0800

    mdb

--- point_clouds/point_cloud_vae_chamfer.py
+++ point_clouds/point_cloud_vae_chamfer.py
@@ -66,13 +66,14 @@ class VariationalAutoencoder(object):
         # Adding 1e-10 to avoid evaluation of log(0.0)
 
         _1,_2,_3,_4 = nn_distance(self.x_reconstr,self.x)
-        reconstr_loss = tf.reduce_sum(_1) + tf.reduce_sum(_3)
+        self.reconstr_loss = tf.reduce_sum(_1) + tf.reduce_sum(_3)
 
         # Regularize posterior towards unit Gaussian prior:
         latent_loss = -0.5 * tf.reduce_sum(1 + self.z_log_sigma_sq - tf.square(self.z_mean) - tf.exp(self.z_log_sigma_sq), 1)
 
-        self.cost = tf.reduce_mean(reconstr_loss) + tf.reduce_mean(latent_loss)   # average over batch
+        self.cost = tf.reduce_mean(self.reconstr_loss) + tf.reduce_mean(latent_loss)   # average over batch
         # Use ADAM optimizer
+
         self.optimizer = \
             tf.train.AdamOptimizer(learning_rate=self.conf.learning_rate).minimize(self.cost)
 
@@ -80,8 +81,8 @@ class VariationalAutoencoder(object):
         """Train model based on mini-batch of input data.
         Return cost of mini-batch.
         """
-        _, cost = self.sess.run((self.optimizer, self.cost), feed_dict={self.x: X})
-        return cost
+        _, cost, cost_rec = self.sess.run((self.optimizer, self.cost, self.reconstr_loss), feed_dict={self.x: X})
+        return cost,cost_rec
 
     def transform(self, X):
         """Transform data by mapping it into the latent space."""
@@ -111,29 +112,30 @@ class VariationalAutoencoder(object):
         batch_size = configuration.batch_size
         n_batches = int(n_examples / batch_size)
         start_time = time.time()
+        epoch_cost_rec = 0.0
 
          # Loop over all batches
         for i in xrange(n_batches):
             batch_i, _, _ = train_data.next_batch(batch_size)
             batch_i = batch_i.reshape([batch_size] + configuration.n_input)
-            #batch_i += .5
-            #batch_i = np.max(1e-10, batch_i)
-            cost = model.partial_fit(batch_i)
+            cost,cost_rec = model.partial_fit(batch_i)
             # Compute average loss
             epoch_cost += cost
+            epoch_cost_rec += cost_rec
 
         epoch_cost /= (n_batches * batch_size)
+        epoch_cost_rec /= (n_batches * batch_size)
         duration = time.time() - start_time
-        return epoch_cost, duration
+        return epoch_cost,epoch_cost_rec,duration
 
     def train_vae(self, train_data ,configuration, loss_display_step=1):
         # Training cycle
         c = configuration
         batch_size = c.batch_size
         for epoch in range(c.training_epochs):
-            cost, duration = self._single_epoch_train(train_data,c)
+            cost,cost_rec,duration = self._single_epoch_train(train_data,c)
             if epoch % loss_display_step == 0:
-                print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(cost))
+                print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(cost),"rec_cost=","{:.9f}".format(cost_rec))
             # Save the model checkpoint periodically.
             if c.saver_step is not None and epoch % c.saver_step == 0:
                 checkpoint_path = os.path.join(c.train_dir, 'model.ckpt')
