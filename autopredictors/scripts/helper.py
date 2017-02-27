7235a7cf2efc43069b7d8d8dd804e8abdbb2fbf8
Panos <optas@stanford.edu>
2017-02-26 23:20:21 -0800

    update more categories

--- autopredictors/scripts/helper.py
+++ autopredictors/scripts/helper.py
@@ -24,7 +24,8 @@ shape_net_core_synth_id_to_category = {
     '02828884': 'bench', '02834778': 'bicycle', '02843684': 'birdhouse',
     '02871439': 'bookshelf', '02876657': 'bottle', '02880940': 'bowl',
     '02924116': 'bus', '02933112': 'cabinet', '02942699': 'camera',
-    '02946921': 'tin_can', '02954340': 'cap', '02958343': 'car',
+    '04401088': 'telephone', '02946921': 'tin_can', '04460130': 'tower',
+    '04468005': 'train', '02954340': 'cap', '02958343': 'car',
     '03001627': 'chair', '03046257': 'clock', '03085013': 'keyboard',
     '03207941': 'dishwasher', '03211117': 'display', '03261776': 'earphone',
     '03325088': 'faucet', '03337140': 'file', '03467517': 'guitar',
@@ -35,7 +36,7 @@ shape_net_core_synth_id_to_category = {
     '03938244': 'pillow', '03948459': 'pistol', '03991062': 'pot',
     '04004475': 'printer', '04074963': 'remote_control', '04090263': 'rifle',
     '04099429': 'rocket', '04225987': 'skateboard', '04256520': 'sofa',
-    '04330267': 'stove', '04379243': 'table'
+    '04330267': 'stove', '04379243': 'table', '04530566': 'vessel', '04554684': 'washer'
 }
 
 
