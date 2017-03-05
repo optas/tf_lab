aa7f7c8cac542020505e34ace450e3ea3c2fc43e
Panos <optas@stanford.edu>
2017-03-04 23:42:00 -0800

    add loss.py

--- fundamentals/loss.py
+++ fundamentals/loss.py
@@ -26,3 +26,13 @@ class Loss():
             entropy = tf.nn.softmax_cross_entropy_with_logits(prediction, ground_truth, name='cross_entropy_per_example')
 
         return tf.reduce_mean(entropy, name='cross_entropy')
+
+    @staticmethod
+    def cosine_distance_loss(prediction,ground_truth):
+        """ 
+        The prediction and ground_truth : [batch, num point, 3]
+
+	""" 
+        cosine =  tf.reduce_sum(prediction * ground_truth,2)   
+        norm = tf.sqrt(tf.reduce_sum(prediction * prediction,2))
+        return tf.reduce_mean( -1.0 * tf.abs( cosine / norm) )
