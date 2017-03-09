f60ade16b96daaa67e68e66646e508f656e413ca
Panos <optas@stanford.edu>
2017-03-08 23:47:43 -0800

    mdb

--- autopredictors/scripts/generate_pclouds.py
+++ autopredictors/scripts/generate_pclouds.py
@@ -47,10 +47,12 @@ def only_uniform_sampling(mesh_file, out_folder, n_samples, swap_y_z=True, dtype
     in_mesh = cleaning.clean_mesh(in_mesh)
     ss_points, _ = in_mesh.sample_faces(n_samples)
     pc = Point_Cloud(points=ss_points.astype(dtype))
-    pc = pc.center_in_unit_sphere()
+#     pc, affine_trans = pc.center_in_unit_sphere(ret_trans=True)
     pc, _ = pc.lex_sort()
     out_file = osp.join(out_folder, model_id + pts_ext)
     np.savetxt(out_file, pc.points)
+#     out_file = osp.join(out_folder, model_id + '_affine_trans.txt')
+#     np.savetxt(out_file, affine_trans)
 
 
 def uniform_sampling_with_normals(mesh_file, out_folder, n_samples, swap_y_z=True, normalize=True, dtype=np.float32):
