d8c22d845ca1d843aba79a206bbc4ea5fc423b0c
Panos <optas@stanford.edu>
2017-03-30 22:17:35 -0700

    mdb

--- nips/model_net.py
+++ nips/model_net.py
@@ -2,23 +2,39 @@
 Created on Mar 30, 2017
 
 @author: optas
+
+Associate code and data for manipulating the shaped of Model-Net-10 (and 40).
+
 '''
 import re
 import os.path as osp
 import numpy as np
 from geo_tool import Mesh, Point_Cloud
+from geo_tool.in_out.soup import load_ply
 import geo_tool.solids.mesh_cleaning as cleaning
 
+
+train_ply_pattern = '(.*)train(.*)\.ply$'
+test_ply_pattern = '(.*)train(.*)\.ply$'
 
 rotation_angles = {'bed': -90, 'desk': -90, 'dresser': 180, 'chair': -90, 'night_stand': 180,     # These aligns model-net-10 with ShapeNetCore.
                    'sofa': 180, 'monitor': 180, 'bathtub': 0, 'table': 0, 'toilet': -90}
 
 
-def file_to_category(full_file):
-    regex = '([_a-z]+)_[0-9]+\.obj$'
+def file_to_category(full_file, ending='obj'):
+    regex = '([_a-z]+)_[0-9]+\.' + ending + '$'
     regex = re.compile(regex)
     s = regex.search(osp.basename(full_file))
     return s.groups()[0]
+
+
+def pc_loader(ply_file):
+    ending = 'ply'
+    drop = len(ending) + 1
+    category = file_to_category(ply_file, ending)
+    model_id = osp.basename(ply_file)
+    model_id = model_id[:-drop]
+    return load_ply(ply_file), model_id, category
 
 
 def pc_sampler(mesh_file, n_samples, save_file=None, dtype=np.float32):
