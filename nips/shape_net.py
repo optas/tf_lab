d9b0881ce8cb8d6e28b4cb4a3f40667f750e848c
Panos <optas@stanford.edu>
2017-04-04 16:02:04 -0700

    mdb

--- nips/shape_net.py
+++ nips/shape_net.py
@@ -12,6 +12,6 @@ from geo_tool.in_out.soup import load_ply
 
 def pc_loader(f_name):
     tokens = f_name.split('/')
-    model_id = tokens[-1].split('_')[0]
+    model_id = tokens[-1].split('.')[0]
     synet_id = tokens[-2]
     return load_ply(f_name), model_id, synet_id
