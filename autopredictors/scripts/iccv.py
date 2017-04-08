5af860891cd41aa42558b211c453e1d9601ad3cb
Panos <optas@stanford.edu>
2017-04-07 22:37:17 -0700

    mdb

--- autopredictors/scripts/iccv.py
+++ autopredictors/scripts/iccv.py
@@ -5,15 +5,4 @@ Created on Mar 22, 2017
 '''
 
 
-def plotting_default_params(category):
-    kwdict = {}
-    kwdict['in_u_sphere'] = in_u_sphere_plotting[category]
-    kwdict['azim'] = azimuth_angles[category]
-    kwdict['color'] = plotting_color[category]
-    return kwdict
 
-in_u_sphere_plotting = {'chair': True, 'airplane': True, 'cabinet': True, 'car': True, 'lamp': True, 'sofa': True, 'table': True, 'vessel': True}
-
-azimuth_angles = {'chair': -50, 'airplane': 0, 'cabinet': -40, 'car': -60, 'lamp': 0, 'sofa': -60, 'table': 60, 'vessel': -60}
-
-plotting_color = {'chair': 'g', 'airplane': 'b', 'cabinet': 'black', 'car': 'r', 'lamp': 'yellow', 'sofa': 'magenta', 'table': 'cyan', 'vessel': [0, 0.6, 1]}
--- autopredictors/scripts/virt_scan_data.py
+++ autopredictors/scripts/virt_scan_data.py
@@ -19,6 +19,20 @@ _n_samples = 2048
 
 all_classes = ['airplane', 'cabinet', 'car', 'chair', 'lamp', 'sofa', 'table', 'vessel']    # All classes used in Angela et al.
 
+in_u_sphere_plotting = {'chair': True, 'airplane': True, 'cabinet': True, 'car': True, 'lamp': True, 'sofa': True, 'table': True, 'vessel': True}
+
+azimuth_angles = {'chair': -50, 'airplane': 0, 'cabinet': -40, 'car': -60, 'lamp': 0, 'sofa': -60, 'table': 60, 'vessel': -60}
+
+plotting_color = {'chair': 'g', 'airplane': 'b', 'cabinet': 'black', 'car': 'r', 'lamp': 'yellow', 'sofa': 'magenta', 'table': 'cyan', 'vessel': [0, 0.6, 1]}
+
+
+def plotting_default_params(category):
+    kwdict = {}
+    kwdict['in_u_sphere'] = in_u_sphere_plotting[category]
+    kwdict['azim'] = azimuth_angles[category]
+    kwdict['color'] = plotting_color[category]
+    return kwdict
+
 
 def permissible_dictionary(file_with_ids):
     ''' Returns a dictionary with model_ids that are white-listed in the input file.
