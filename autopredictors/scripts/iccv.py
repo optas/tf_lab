c5df36e09d0e04c3ef7027e487b0622cc6163f12
Panos <optas@stanford.edu>
2017-04-07 22:35:35 -0700

    mdb

--- autopredictors/scripts/iccv.py
+++ autopredictors/scripts/iccv.py
@@ -4,49 +4,16 @@ Created on Mar 22, 2017
 @author: optas
 '''
 
-import numpy as np
-import os.path as osp
-import matplotlib.pyplot as plt
 
+def plotting_default_params(category):
+    kwdict = {}
+    kwdict['in_u_sphere'] = in_u_sphere_plotting[category]
+    kwdict['azim'] = azimuth_angles[category]
+    kwdict['color'] = plotting_color[category]
+    return kwdict
 
 in_u_sphere_plotting = {'chair': True, 'airplane': True, 'cabinet': True, 'car': True, 'lamp': True, 'sofa': True, 'table': True, 'vessel': True}
 
 azimuth_angles = {'chair': -50, 'airplane': 0, 'cabinet': -40, 'car': -60, 'lamp': 0, 'sofa': -60, 'table': 60, 'vessel': -60}
 
-plotting_color = {'chair': 'g',
-                  'airplane': 'b',
-                  'cabinet': 'black',
-                  'car': 'r',
-                  'lamp': 'yellow',
-                  'sofa': 'magenta',
-                  'table': 'cyan',
-                  'vessel': [0, 0.6, 1]}
-
-
-def plot_3d_point_cloud(pc_cloud, show=True, in_u_sphere=False, axis=None, elev=10, azim=240,
-                        marker='.', s=8, alpha=.8, figsize=(5, 5), dot_color=['blue', 'blue', 'blue'],
-                        *args, **kwargs):
-
-    if axis is None:
-        fig = plt.figure(figsize=figsize)
-        ax = fig.add_subplot(111, projection='3d')
-    else:
-        ax = axis
-        fig = axis
-
-    x = pc_cloud.points[:, 0]
-    y = pc_cloud.points[:, 1]
-    z = pc_cloud.points[:, 2]
-
-    ax.scatter(x, y, z, marker=marker, s=s, alpha=alpha, color=dot_color, *args, **kwargs)
-    ax.view_init(elev=elev, azim=azim)
-    plt.axis('off')
-
-    if in_u_sphere:
-        ax.set_xlim3d(-0.5, 0.5)
-        ax.set_ylim3d(-0.5, 0.5)
-        ax.set_zlim3d(-0.5, 0.5)
-
-    if show:
-        plt.show()
-    return fig
+plotting_color = {'chair': 'g', 'airplane': 'b', 'cabinet': 'black', 'car': 'r', 'lamp': 'yellow', 'sofa': 'magenta', 'table': 'cyan', 'vessel': [0, 0.6, 1]}
