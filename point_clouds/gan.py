9d33ddc2cf8f70a772e701607f1e783e298bd005
Panos <optas@stanford.edu>
2017-05-03 12:09:53 -0700

    mdb

--- point_clouds/gan.py
+++ point_clouds/gan.py
@@ -6,6 +6,7 @@ Created on May 3, 2017
 
 import os.path as osp
 import warnings
+import numpy as np
 import tensorflow as tf
 
 from general_tools.in_out.basics import create_dir
@@ -32,6 +33,13 @@ class GAN(object):
         feed_dict = {self.noise: noise}
         return self.sess.run([self.generator_out], feed_dict=feed_dict)
 
+    def generate_many(self, n_samples, sigma=1):
+        gen_samples = np.zeros(([n_samples] + self.n_output))
+        for i in xrange(n_samples):
+            pc_gen = self.generate(self.generator_noise_distribution(1, self.noise_dim, sigma=sigma))[0][0]
+            gen_samples[i] = pc_gen
+        return gen_samples
+
     def restore_model(self, model_path, epoch, verbose=False):
         '''Restore all the variables of a saved model.
         '''
@@ -42,25 +50,3 @@ class GAN(object):
         else:
             if verbose:
                 print('Model restored in epoch {0}.'.format(epoch))
-
-    def train(self, train_data, configuration):
-        c = configuration
-        stats = []
-
-        if c.saver_step is not None:
-            create_dir(c.train_dir)
-
-        for _ in xrange(c.training_epochs):
-            loss, duration = self._single_epoch_train(train_data, c)
-            epoch = int(self.sess.run(self.epoch.assign_add(tf.constant(1.0))))
-            stats.append((epoch, loss, duration))
-
-            if epoch % c.loss_display_step == 0:
-                print("Epoch:", '%04d' % (epoch), 'training time (minutes)=', "{:.4f}".format(duration / 60.0), "loss=", "{:.9f}".format(loss))
-
-            # Save the models checkpoint periodically.
-            if c.saver_step is not None and (epoch % c.saver_step == 0 or epoch - 1 == 0):
-                checkpoint_path = osp.join(c.train_dir, model_saver_id)
-                self.saver.save(self.sess, checkpoint_path, global_step=self.epoch)
-
-        return stats
