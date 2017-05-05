a6c8e5d7764840c767014e2f23253f2a0e081902
Panos <optas@stanford.edu>
2017-05-04 17:34:06 -0700

    μδβ

--- point_clouds/raw_wgan.py
+++ point_clouds/raw_wgan.py
@@ -63,12 +63,6 @@ class RawWGAN(GAN):
                 self.opt_d = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(self.loss_d, var_list=d_params)
                 self.opt_g = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(self.loss_g, var_list=g_params)
 
-#                 self.opt_d = tf.train.AdagradOptimizer(learning_rate=learning_rate).minimize(self.loss_d, var_list=d_params)
-#                 self.opt_g = tf.train.AdagradOptimizer(learning_rate=learning_rate).minimize(self.loss_g, var_list=g_params)
-
-#                 self.opt_d = self.optimizer(learning_rate, self.loss_d, d_params)
-#                 self.opt_g = self.optimizer(learning_rate, self.loss_g, g_params)
-
                 self.saver = tf.train.Saver(tf.global_variables(), max_to_keep=None)
                 self.init = tf.global_variables_initializer()
 
