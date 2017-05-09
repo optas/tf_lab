77c8b70457f063fc3e7320a5239029067419908e
Panos <optas@stanford.edu>
2017-05-08 23:52:49 -0700

    mdb

--- nips/helper.py
+++ nips/helper.py
@@ -30,3 +30,14 @@ def compute_3D_sphere(resolution=32):
     pc = Point_Cloud(pts).center_in_unit_sphere()
     pts = pc.points
     return pts, spacing
+
+
+def zero_mean_half_sphere(in_pclouds):    
+    ''' Zero MEAN + Max_dist = 0.5
+    '''
+    pclouds = in_pclouds.copy()
+    pclouds = pclouds - np.expand_dims(np.mean(pclouds, axis=1), 1)
+    dist = np.max(np.sqrt(np.sum(pclouds ** 2, axis=2)), 1)
+    dist = np.expand_dims(np.expand_dims(dist, 1), 2)
+    pclouds = pclouds / (dist * 2.0)
+    return pclouds
