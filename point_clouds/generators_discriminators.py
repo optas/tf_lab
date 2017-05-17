dec05d3280c6ce9e2a7c83fefdd3b25877ed68a2
Panos <optas@stanford.edu>
2017-05-17 16:03:48 -0700

    mdb

--- point_clouds/generators_discriminators.py
+++ point_clouds/generators_discriminators.py
@@ -74,7 +74,7 @@ def latent_code_generator_two_layers(z, out_dim, layer_sizes=[128], b_norm=True)
     return out_signal
 
 
-def latent_code_discriminator_three_layers(in_singnal, layer_sizes=[256, 256, 512], b_norm=True, non_linearity=tf.nn.relu, reuse=False, scope=None):
+def latent_code_discriminator_two_layers(in_singnal, layer_sizes=[256, 512], b_norm=True, non_linearity=tf.nn.relu, reuse=False, scope=None):
     layer_sizes = layer_sizes + [1]
     d_logit = decoder_with_fc_only(in_singnal, layer_sizes=layer_sizes, non_linearity=non_linearity, b_norm=b_norm, reuse=reuse, scope=scope)
     d_prob = tf.nn.sigmoid(d_logit)
