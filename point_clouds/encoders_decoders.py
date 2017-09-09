2fdec8b386f2b387c319a147c2b32917be32ec20
Panos <optas@stanford.edu>
2017-09-09 00:44:23 -0700

    flexible encoder

--- point_clouds/encoders_decoders.py
+++ point_clouds/encoders_decoders.py
@@ -61,7 +61,8 @@ def encoder_with_convs_and_symmetry(in_signal, n_filters=[64, 128, 256, 1024], f
         if dropout_prob is not None and dropout_prob[i] > 0:
             layer = dropout(layer, 1.0 - dropout_prob[i])
 
-    layer = symmetry(layer, axis=1)
+    if symmetry is not None:
+        layer = symmetry(layer, axis=1)
     return layer
 
 
