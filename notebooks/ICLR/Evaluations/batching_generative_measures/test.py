868f6d5fd864ca3189ce25cf754fcf4f88776cf0
Panos Achlioptas <optas@stanford.edu>
2017-10-11 14:01:54 -0700

    mdb

---
+++ notebooks/ICLR/Evaluations/batching_generative_measures/test.py
@@ -0,0 +1,7 @@
+import torch
+from torch.autograd import Variable
+print "yo"
+
+from pcloud_benchmark.nndistance.modules.nnd import NNDModule
+
+
---
+++ notebooks/ICLR/Evaluations/batching_generative_measures/test.sh
@@ -0,0 +1,1 @@
+/orions4-zfs/projects/optas/Virt_Env/tf_1_plus/bin/python test.py
