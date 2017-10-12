2602accc93e8dad9f4cd446a07426391b95b641f
Panos Achlioptas <optas@stanford.edu>
2017-10-11 22:16:05 -0700

    mdb

--- notebooks/ICLR/Evaluations/batching_generative_measures/test.py
+++
@@ -1,5 +0,0 @@
-try:
-    import torch
-    from torch.autograd import Variable
-except:
-    print('PyTorch not working. MMD measurement won\'t be available')
--- notebooks/ICLR/Evaluations/batching_generative_measures/test.sh
+++
@@ -1,1 +0,0 @@
-/orions4-zfs/projects/optas/Virt_Env/tf_1_plus/bin/python test.py
