b478d8e4927c056dc652ea4e95c721a1263342ce
Panos <optas@stanford.edu>
2017-10-19 14:55:19 -0700

    mdb

--- notebooks/ICLR/Evaluations/evaluator.py
+++ notebooks/ICLR/Evaluations/evaluator.py
@@ -7,27 +7,36 @@ import os.path as osp
 from tf_lab.nips.helper import center_pclouds_in_unit_sphere
 from tf_lab.evaluate.generative_pc_nets import entropy_of_occupancy_grid, jensen_shannon_divergence, minimum_mathing_distance
 
+
 def identity(x):
     return x
 
+
 def sampling_mmd(sample_data, ref_data, n_samples=10, ref_pop_size=50, sample_pop_size=None, emd=True, verbose=False):
-    
-    ref_ids = np.arange(len(ref_data))
-    sample_ids = np.arange(len(sample_data))
-   
+
+    n_ref = len(ref_data)
+    n_sample = len(sample_data)
+    ref_ids = np.arange(n_ref)
+    sample_ids = np.arange(n_sample)
+
     if sample_pop_size is None:
         sample_pop_size = ref_pop_size
-    
-    scores = []    
+
+    normalize = True
+    if emd:
+        normalize = False
+
+    scores = []
     for i in range(n_samples):
         if verbose:
-            print(i)        
-        sb_ref = np.random.choice(ref_ids, min(ref_pop_size, len(ref_ids)), replace=False)
-        sb_sam = np.random.choice(sample_ids, min(sample_pop_size, len(sample_ids)), replace=False)        
-        mi = minimum_mathing_distance(sample_data[sb_sam], ref_data[sb_ref], sample_pop_size, normalize=True, use_EMD=emd)
+            print(i)
+        sb_ref = np.random.choice(ref_ids, ref_pop_size, replace=False)
+        sb_sam = np.random.choice(sample_ids, sample_pop_size, replace=False)
+        mi = minimum_mathing_distance(sample_data[sb_sam], ref_data[sb_ref], sample_pop_size, normalize=normalize, use_EMD=emd)
         scores.append(mi[0])
     scores = np.array(scores)
     return scores
+
 
 class Evaluator():
     def __init__(self, class_name, voxel_res=28, jsd_in_sphere=True, norm=identity):
@@ -36,18 +45,18 @@ class Evaluator():
         self.voxel_res = voxel_res
         self.jsd_in_sphere = jsd_in_sphere
         self.splits = ['train', 'test', 'val']
-        
+
     def load_gt_data(self):
         # Load Ground-Truth Data.
         top_gt_dir = '/orions4-zfs/projects/optas/DATA/OUT/iclr/evaluations/gt_data/'
         gt_data = {}
         for s in self.splits:
-            gt_file = osp.join(top_gt_dir, self.class_name + '_'+ s +'.npz')
+            gt_file = osp.join(top_gt_dir, self.class_name + '_' + s + '.npz')
             gt_load = np.load(gt_file)
             gt_data[s] = self.normalizer(gt_load[gt_load.keys()[0]])
-            
+
         self.gt_data = gt_data
-    
+
     def prepare_gt_grid_variables(self):
         # Prepare Ground-Truth grid-variables for JSD.
         gt_grid_vars = {}
@@ -55,55 +64,53 @@ class Evaluator():
             gt_grid_vars[s] = entropy_of_occupancy_grid(self.gt_data[s], self.voxel_res, self.jsd_in_sphere)[1]
         self.gt_grid_vars = gt_grid_vars
 
-    def prepare_sample_data(self, sample_file, boost_sample=5, random_seed=None):
+    def prepare_sample_data(self, sample_file, boost_sample=1, random_seed=None):
         sample_data = {}
-        sample_load = np.load(sample_file)      
+        sample_load = np.load(sample_file)
         sample_data['train'] = self.normalizer(sample_load[sample_load.keys()[0]])
 
         # Sub-sample train sample data, to form samples for test, val.
         n_train = len(sample_data['train'])
         n_test = len(self.gt_data['test'])
         n_val = len(self.gt_data['val'])
-        
+
         if random_seed is not None:
             np.random.seed(random_seed)
-            
-        test_idx = np.random.choice(np.arange(n_train), boost_sample*n_test)
-        val_idx = np.random.choice(np.arange(n_train), boost_sample*n_val)
+
+        test_idx = np.random.choice(np.arange(n_train), boost_sample * n_test)
+        val_idx = np.random.choice(np.arange(n_train), boost_sample * n_val)
         sample_data['test'] = sample_data['train'][test_idx]
         sample_data['val'] = sample_data['train'][val_idx]
         self.sample_data = sample_data
 
-    def compute_jsd(self, f_out=sys.stdout):        
+    def compute_jsd(self, f_out=sys.stdout):
         for s in self.splits:
             sample_grid_var = entropy_of_occupancy_grid(self.sample_data[s], self.voxel_res, self.jsd_in_sphere)[1]
             jsd_score = jensen_shannon_divergence(sample_grid_var, self.gt_grid_vars[s])
             print(s, jsd_score, file=f_out)
             if f_out != sys.stdout:
                 print(s, jsd_score)
-            
-           
-    def compute_mmd(self, loss='chamfer', sample_estimator=False, n_samples=5, 
-                    ref_pop_size=50, sample_pop_size=None,
+
+    def compute_mmd(self, loss='chamfer', sample_estimator=False, n_samples=5, ref_pop_size=50, sample_pop_size=None,
                     f_out=sys.stdout, skip=[], batch_size=None):
         if loss == 'emd':
             emd = True
-        elif loss == 'chamfer':           
+        elif loss == 'chamfer':
             emd = False
         else:
             assert(False)
-        
+
         for s in self.splits:
             if s in skip:
                 continue
-                
+
             if sample_estimator:
                 scores = sampling_mmd(self.sample_data['train'], self.gt_data[s], n_samples, ref_pop_size, sample_pop_size, emd=emd)
             else:
                 if batch_size is None and not emd:
-                    batch_size = len(self.sample_data[s]) # use all in Chamfer                
+                    batch_size = len(self.sample_data[s])   # use all in Chamfer.
                 scores = minimum_mathing_distance(self.sample_data[s], self.gt_data[s], batch_size, normalize=True, use_EMD=emd)[1]
-    
+
             print(s, np.mean(scores), np.std(scores), file=f_out)
             if f_out != sys.stdout:
                 print(s, np.mean(scores), np.std(scores))
