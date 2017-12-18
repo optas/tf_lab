acdefd86e56762bca3e09030f7b9bef19c1f980a
Panos <optas@stanford.edu>
2017-12-18 12:01:48 -0800

    mdb

--- voxels/soup.py
+++ voxels/soup.py
@@ -3,15 +3,21 @@ Created on December 14, 2017
 
 @author: optas
 '''
-
-from multiprocessing import Pool
-import matplotlib.pyplot as plt
 import numpy as np
 import warnings
+import pickle
+import os.path as osp
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from skimage import measure
-import os.path as osp
+
 from external_tools.binvox_rw.binvox_rw import read_as_3d_array
+from general_tools.simpletons import invert_dictionary
+
+from .. in_out.basics import Data_Splitter
+from .. data_sets.numpy_dataset import NumpyDataset
+from .. data_sets.shape_net import snc_category_to_synth_id
 
 
 def plot_isosurface(voxel_grid, iso_val=0):
@@ -46,22 +52,7 @@ def read_bin_vox_file(binvox_filename, perm_axis=(0, 1, 2), expand_last_dim=True
     return vol
 
 
-def read_shape_net_bin_vox(binvox_filename):
-    tokens = binvox_filename.split('/')
-    model_id = tokens[-2]
-    class_id = tokens[-3]
-    vol_grid = read_bin_vox_file(binvox_filename, perm_axis=(0, 2, 1))
-    return vol_grid, model_id, class_id
-
-
-def read_tartachenko_car_bin_vox(binvox_filename):
-    model_id = osp.basename(binvox_filename)
-    model_id = model_id.split('.')[0]
-    vol_grid = read_bin_vox_file(binvox_filename, perm_axis=(0, 2, 1))
-    return vol_grid, model_id, 'car'
-
-
-def load_voxel_grids_from_filenames(file_names, n_threads=1, loader=read_shape_net_bin_vox, verbose=False, dtype=np.float32):
+def load_voxel_grids_from_filenames(file_names, loader, n_threads=1, verbose=False, dtype=np.float32):
     assert(len(file_names) > 1)
     voxel_grid, _, _ = loader(file_names[0])
     voxel_dims = list(voxel_grid.shape)
@@ -83,3 +74,86 @@ def load_voxel_grids_from_filenames(file_names, n_threads=1, loader=read_shape_n
         print('{0} voxel-grids were loaded. They belong in {1} shape-classes.'.format(len(voxel_grids), len(np.unique(class_ids))))
 
     return voxel_grids, model_names, class_ids
+
+
+def read_shape_net_bin_vox(binvox_filename):
+    tokens = binvox_filename.split('/')
+    model_id = tokens[-2]
+    class_id = tokens[-3]
+    vol_grid = read_bin_vox_file(binvox_filename, perm_axis=(0, 2, 1))
+    return vol_grid, model_id, class_id
+
+
+def read_tartachenko_car_bin_vox(binvox_filename):
+    model_id = osp.basename(binvox_filename)
+    model_id = model_id.split('.')[0]
+    vol_grid = read_bin_vox_file(binvox_filename, perm_axis=(0, 2, 1))
+    return vol_grid, model_id, 'car'
+
+
+def read_phuoc_bin_vox(in_f):
+    model_id = osp.basename(in_f)[len('model_normalized_'):-len('_clean.binvox')]
+    vol_grid = read_bin_vox_file(in_f, perm_axis=(0, 2, 1))
+    return vol_grid, model_id, 'chair'
+
+
+def load_data_for_rebuttal(load_tartachenko, load_phuoc, class_name, resolution):
+    top_data_dir = '/orions4-zfs/projects/optas/DATA'
+
+    in_data = dict()
+    if load_tartachenko:
+        class_name = 'car'
+        top_voxel_dir = osp.join(top_data_dir, 'Voxels/Tartatchenko_Car_Voxels/sn_car_binvox_' + str(resolution))
+        splits = ['train', 'test']
+        data_in_split = dict()
+        data_in_split['train'] = range(6000)
+        data_in_split['test'] = range(6000, 7497)
+        for s in splits:
+            data_in_split_full_path = []
+            for d in data_in_split[s]:
+                d_file = osp.join(top_voxel_dir, '%04d' % (d,) + '.binvox')
+                if osp.exists(d_file):
+                    data_in_split_full_path.append(d_file)
+            voxel_grids, model_names, class_ids = load_voxel_grids_from_filenames(data_in_split_full_path, loader=read_tartachenko_car_bin_vox,
+                                                                                  n_threads=10)
+            in_data[s] = NumpyDataset([voxel_grids, class_ids + '_' + model_names], ['voxels', 'labels'])
+    else:
+        syn_id = snc_category_to_synth_id()[class_name]
+        top_voxel_dir = osp.join(top_data_dir, 'Voxels/Choy_ShapeNetVox32')
+        if load_phuoc:
+            resolution = 64
+            loader = read_phuoc_bin_vox
+            top_voxel_dir = osp.join(top_data_dir, 'Voxels/Two_Phuong_Chair_64')
+            phu_dict_file = '/orions4-zfs/projects/optas/DATA/OUT/2d_to_pc/from_phuoc/phuoc_to_sn_map.pickle'
+            with open(phu_dict_file, 'rb') as fin:
+                phuoc_to_sn_map = pickle.load(fin)
+                sn_id_to_phuoc = invert_dictionary(phuoc_to_sn_map)
+        else:
+            loader = read_shape_net_bin_vox
+
+        splitter = Data_Splitter(top_voxel_dir, data_file_ending='.binvox')
+        splits = ['train', 'test', 'val']
+        in_data = dict()
+
+        for s in splits:
+            split_file = osp.join(top_data_dir, 'Point_Clouds/Shape_Net/Splits/single_class_splits/' + syn_id + '/85_5_10/', s + '.txt')
+            data_in_split = splitter.load_splits(split_file, full_path=False)
+            data_in_split_full_path = []
+            for d in data_in_split:
+                _, model_id = d.split('_')
+                if load_phuoc:
+                    ph_code = sn_id_to_phuoc[model_id]
+                    d_file = osp.join(top_voxel_dir, ph_code + '_clean.binvox')
+                else:
+                    d_file = osp.join(top_voxel_dir, syn_id, model_id, 'model.binvox')
+
+                if osp.exists(d_file):
+                    data_in_split_full_path.append(d_file)
+                else:
+                    print 'missing:', d_file 
+
+            voxel_grids, model_names, class_ids = load_voxel_grids_from_filenames(data_in_split_full_path, loader=loader,
+                                                                                  n_threads=10)
+            in_data[s] = NumpyDataset([voxel_grids, class_ids + '_' + model_names], ['voxels', 'labels'])
+
+    return in_data
