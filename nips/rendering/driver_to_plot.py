52c0084a80f785b519df0757c5fa8ef34a53409f
Panos <optas@stanford.edu>
2018-03-15 14:36:15 -0700

    bye bye nips

--- nips/rendering/driver_to_plot.py
+++
@@ -1,8 +0,0 @@
-cmap = plt.cm.get_cmap("hsv", 10)
-cmap = np.array([cmap(i) for i in range(10)])[:,:3]
-
-color = cmap[np.array([0] * 2500 + [2] * 2000), :]
-
-point_np = points.transpose(2,1).data.numpy()
-
-showpoints(point_np, color)
