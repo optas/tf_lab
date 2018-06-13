df178f25c01f63704f905ebf722aef953d75c9c3
Panos <optas@stanford.edu>
2018-06-13 12:03:43 -0700

    mdb

--- neural_net.py
+++ neural_net.py
@@ -28,9 +28,9 @@ class Neural_Net(object):
         with tf.variable_scope(name):
             with tf.device('/cpu:0'):
                 self.epoch = tf.get_variable('epoch', [], initializer=tf.constant_initializer(0), trainable=False)
-                
+
             self.increment_epoch = self.epoch.assign_add(tf.constant(1.0))
-        
+
         self.no_op = tf.no_op()
 
     def is_training(self):
