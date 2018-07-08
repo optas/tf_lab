890a71f88fe5d127ad10192f4e52d5f1e1af7b7a
Panos Achlioptas <optas@stanford.edu>
2018-07-07 20:33:01 -0700

    mdb

--- external/structural_pc_losses.py
+++ external/structural_pc_losses.py
@@ -9,12 +9,14 @@ import tensorflow as tf
 try:
     losses_found = True
     if tf.__version__ == '1.6.0':
-	print '1.6'
-    	from .. external.Chamfer_EMD_tf16.tf_nndistance import nn_distance
-    	from .. external.Chamfer_EMD_tf16.tf_approxmatch import approx_match, match_cost
-    else:
+        from .. external.Chamfer_EMD_tf16.tf_nndistance import nn_distance
+        from .. external.Chamfer_EMD_tf16.tf_approxmatch import approx_match, match_cost
+    elif tf.__version__ == '1.3.0':
         from .. external.Chamfer_EMD_tf1plus.tf_nndistance import nn_distance
         from .. external.Chamfer_EMD_tf1plus.tf_approxmatch import approx_match, match_cost
+    else:
+        losses_found = False
+        print('External Losses (Chamfer-EMD) cannot be loaded.')
 except:
     losses_found = False
     print('External Losses (Chamfer-EMD) cannot be loaded.')
