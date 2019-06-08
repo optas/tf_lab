1908b37c80a27df0f4cc011072883561edc6a658
Panos <optas@stanford.edu>
2019-06-08 16:23:30 -0700

    mdb

--- __init__.py
+++ __init__.py
@@ -1,5 +1,14 @@
+import warnings
+
 try:
-    from . data_sets.numpy_dataset import NumpyDataset
-    from . neural_net import Neural_Net
+    from . data_sets.numpy_dataset import NumpyDataset    
 except:
-    pass
+    warnings.warn('Cannot load NumpyDataset.')
+
+
+try:
+    from . neural_net import Neural_Net    
+except:
+    warnings.warn('Neural_Net')
+
+    
