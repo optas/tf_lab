737d370a9c7c5aee6fcc2db6ccd5f1ba0e88db2f
Panos <optas@stanford.edu>
2019-09-06 20:05:30 -0700

    mdb

--- images/encoders_decoders.py
+++ images/encoders_decoders.py
@@ -55,14 +55,12 @@ def conv_based_encoder(in_signal, n_filters, filter_sizes, strides=[1], b_norm=[
         if verbose:
             print name, 'conv params = ', np.prod(layer.W.get_shape().as_list()) + np.prod(layer.b.get_shape().as_list()),
 
-
         if b_norm[i]:
             name += '_bnorm'
             scope_i = expand_scope_by_name(scope, name)
             layer = batch_normalization(layer, name=name, reuse=reuse, scope=scope_i)
             if verbose:
                 print('bnorm params = ', np.prod(layer.beta.get_shape().as_list()) + np.prod(layer.gamma.get_shape().as_list()))
-                        
 
         if non_linearity is not None:
             layer = non_linearity(layer)
